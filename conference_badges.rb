d9e6c745297eb5ac7cfcb508f3c8ed3ec3cf9d6d
brinda behal <brindab50@gmail.com>
2019-04-26 19:22:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  
+ return "Hello, my name is #{name}."
+end 
+def batch_badge_creator(array)
+  badges = []
+  array.each do |name| 
+    badge_maker(name)
+    badges.push(badge_maker(name))
+  end 
+  return badges
+end 
+def assign_rooms(array)
+  rooms = []
+  array.each_with_index do |name, index|
+ # binding.pry
+  rooms.push("Hello, #{name}! You'll be assigned to room #{index + 1}!")
+  end 
+  return rooms
+end 
+def printer(attendees)
+ puts batch_badge_creator
+ puts assign_rooms
+end
